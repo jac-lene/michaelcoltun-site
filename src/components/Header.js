a857129e1d9d8aff8611d25bdd15d47d67a33fdd
jac-lene <jaclenesini@gmail.com>
2022-10-04 15:12:44 -0400

    changed font and added scroll effect to nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,22 @@ function Nav(props) {
     props.scrollFunction(props.ref)
   }
 
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos < currentScrollPos) {
+    // console.log(prevScrollpos, currentScrollPos)
+    // document.getElementById("navbar").classList.add("fadeout");
+    document.getElementById("navbar").style = ("animation: fadeout .2s ease-in forwards;");
+  } else {
+    document.getElementById("navbar").style = ("animation: fadein .2s ease-in forwards;");
+  }
+  prevScrollpos = currentScrollPos;
+}
+
   return (
-    <div className='fixed'>
-        <nav>
+    <div>
+        <nav className='navbar' id='navbar'>
       
           <div className='mikey-container' onClick={getReference}>
           <div className='mikeyname'>Michael Coltun</div>
