b5921673351a353ba297b9e3b9a7de2641650d68
jac-lene <jaclenesini@gmail.com>
2022-10-03 22:23:09 -0400

    got video url converter working and grid up

--- src/pages/FilmCommAdd.js
+++ src/pages/FilmCommAdd.js
@@ -1,43 +1,124 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './CSS/FilmCommAdd.css'
 
 function FilmCommAdd() {
+    // https://player.vimeo.com/video/254712264?h=768a567cc3&color=e62517
+    // ?h=475052af07&color=e62517"
+
+    // "https://www.youtube.com/embed/Ktd9rvsOL5M
+
+    let videoURL = "https://youtu.be/aPUVUrS2oC0"
+    let convertedURL = ""
+
+    function convertURL (x) {
+        if (x.match(/vimeo/)) {
+            console.log("it's vimeo")
+            const a = x.split("//")
+            console.log(a)
+            a.splice(1, 0, '//player.')
+            console.log(a)
+            const b = a[2].split('/')
+            console.log(b)
+            a.pop()
+            console.log(a)
+            b.splice(1,0, "/video/")
+            console.log(b)
+            const c = b.join('')
+            const d = a.join('')
+            console.log(d, c)
+            convertedURL = d.concat(c)
+            console.log(convertedURL)
+            return (convertedURL)
+        }
+        else if (x.match(/you/)) {
+            console.log("it's youtube")
+            const a = x.split('.')
+            console.log(a)
+            const b = a[1].split('/')
+            a.pop()
+            console.log(a)
+            console.log(b)
+            b.splice(1,0,'.com/embed/')
+            console.log(b)
+            const c = a.join('')
+            const d = b.join('')
+            convertedURL = c.concat(d)
+            console.log(convertedURL)
+            return (convertedURL)
+        }
+        // convertedURL = videoURL.replace('', '')
+    }
+
+    // console.log(convertedURL)
+
+    // useEffect(() => {
+    //     convertURL(videoURL)
+    // }, []
+    // )
+    
 
     const contentList = [ 
         {
-            embedCode: <div><iframe src="https://player.vimeo.com/video/254712264?h=768a567cc3&color=e62517" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-            <p><a href="https://vimeo.com/254712264">Form &amp; Reel</a> from <a href="https://vimeo.com/user6597683">WORKPLAYWORK</a> on <a href="https://vimeo.com">Vimeo</a>.</p></div>,
-            src: "https://www.youtube.com/watch?v=Ktd9rvsOL5M&t=3s&ab_channel=LaurenGreenhall",
+            src: "https://vimeo.com/254712264",
             title: "ZELDA Documentary",
-            artist: "",
+            artist: "Lauren Greenhall",
             publication: "",
             otherInfo: "",
         },
         {
-            embedCode: <div><iframe src="https://player.vimeo.com/video/254712264?h=768a567cc3&color=e62517" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-            <p><a href="https://vimeo.com/254712264">Form &amp; Reel</a> from <a href="https://vimeo.com/user6597683">WORKPLAYWORK</a> on <a href="https://vimeo.com">Vimeo</a>.</p></div>,
-            src: "https://www.youtube.com/watch?v=Ktd9rvsOL5M&t=3s&ab_channel=LaurenGreenhall",
+            src: "https://youtu.be/aPUVUrS2oC0",
+            title: "ZELDA Documentary",
+            artist: "",
+            otherInfo: "",
+        },
+        {
+            src: "https://vimeo.com/55825202",
+            title: "ZELDA Documentary",
+            artist: "Lauren Greenhall",
+            publication: "",
+            otherInfo: "",
+        },
+        {
+            src: "https://youtu.be/aPUVUrS2oC0",
             title: "ZELDA Documentary",
             artist: "",
             publication: "",
             otherInfo: "",
         },
+      
     ]
 
 
   return (
     <div id='fca'>
+
         <div className='subtitle'>film/commercials/ads</div>
 
         <div className='content'>
+               
             { contentList.map((content) => {
+                 {convertURL(content.src)}
                 return (
                     <div className='contentCard'>
-                    <div>{content.embedCode}</div>
+
+                        <iframe
+                        width="400"
+                        height="200"
+                        src={convertedURL}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title="Embedded video"
+                        />
+                        <div>{content.title}</div>
+                        <div>{content.artist}</div>
+
                     </div>
                 )
             })
             }
+
+
         </div>
 
 
