6c35cfcc9979ae68c95b3250556411936f1ec3a2
jac-lene <jaclenesini@gmail.com>
2022-10-04 16:05:02 -0400

    no pointer when not visible

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,9 +12,10 @@ function Nav(props) {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos < currentScrollPos) {
     // console.log(prevScrollpos, currentScrollPos)
-    // document.getElementById("navbar").classList.add("fadeout");
+    document.getElementById("navbar").classList.add("noclick");
     document.getElementById("navbar").style = ("animation: fadeout .2s ease-in forwards;");
   } else {
+    document.getElementById("navbar").classList.remove("noclick");
     document.getElementById("navbar").style = ("animation: fadein .2s ease-in forwards;");
   }
   prevScrollpos = currentScrollPos;
@@ -22,34 +23,33 @@ function Nav(props) {
 
   return (
     <div>
-        <nav className='navbar' id='navbar'>
+     <nav className='navbar' id='navbar'>
       
-          <div className='mikey-container link' onClick={() => {props.handleScroll(props.home.current)}}>
-          <div className='mikeyname'>Michael Coltun</div>
-          </div>
-
+      <div className='mikey-container link' onClick={() => {props.handleScroll(props.home.current)}}>
+      <div className='mikeyname'>Michael Coltun</div>
+      </div>
+     
 <div className='nav'>
 
 <div className='nav-left'>
-    <div className='nav-spacer'></div>
-    <div className='fca link' onClick={() => {props.handleScroll(props.fca.current)}}>film/commercials/ads</div>
-    <div className='sw link' onClick={() => {props.handleScroll(props.select.current)}}>selected work</div>
-    <div className='nav-spacer'></div>
+<div className='nav-spacer'></div>
+<div className='fca link' onClick={() => {props.handleScroll(props.fca.current)}}>film/commercials/ads</div>
+<div className='sw link' onClick={() => {props.handleScroll(props.select.current)}}>selected work</div>
+<div className='nav-spacer'></div>
 </div>
-
-
 
 <div className='nav-right'>
-    <div className='nav-spacer'></div>
-    <div className='live link' onClick={() => {props.handleScroll(props.live.current)}}>live shows</div>
-    <div className='about link' onClick={() => {props.handleScroll(props.about.current)}}>about</div>
-    <div className='nav-spacer'></div>
+<div className='nav-spacer'></div>
+<div className='live link' onClick={() => {props.handleScroll(props.live.current)}}>live shows</div>
+<div className='about link' onClick={() => {props.handleScroll(props.about.current)}}>about</div>
+<div className='nav-spacer'></div>
 </div>
 
 
 
 </div>
-        </nav>
+    </nav>
+        
         
         </div>
   )
