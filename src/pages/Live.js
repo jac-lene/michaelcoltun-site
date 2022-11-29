78f526d0870e0771e9bc9072352ee9d6db875d95
jac-lene <jaclenesini@gmail.com>
2022-11-29 16:14:38 -0500

    live shows

--- src/pages/Live.js
+++ src/pages/Live.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import './CSS/Live.css'
 
 function Live(props) {
   return (
     <div>
         <div className='subtitle'>live</div>
+        <div className='spacer'></div>
+        <h3>UPCOMING</h3>
+        <p>October 30th, 2022<span> : </span>Brooklyn, NY <span> @ </span> Brooklyn Steel w/ Mdou Moctar</p>
+        <p>October 30th, 2022<span> : </span>Brooklyn, NY <span> @ </span> Brooklyn Steel w/ Mdou Moctar</p>
+
+        <h3>PAST</h3>
+
+        <p>October 30th, 2022<span> : </span>Brooklyn, NY <span> @ </span> Brooklyn Steel w/ Mdou Moctar</p>
+        <p>October 30th, 2022<span> : </span>Brooklyn, NY <span> @ </span> Brooklyn Steel w/ Mdou Moctar</p>
+        <p>October 30th, 2022<span> : </span>Brooklyn, NY <span> @ </span> Brooklyn Steel w/ Mdou Moctar</p>
     
-    
-    
+
+        <div className='spacer'></div>
     </div>
   )
 }
