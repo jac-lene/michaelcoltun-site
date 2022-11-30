340a0e06fcbd216617fb66fcae45d61b1fe3241f
jac-lene <jaclenesini@gmail.com>
2022-11-30 10:22:39 -0500

    fake api for shows

--- src/pages/Live.js
+++ src/pages/Live.js
@@ -2,19 +2,55 @@ import React from 'react'
 import './CSS/Live.css'
 
 function Live(props) {
+
+  const showList = [
+    {
+      upcoming: true,
+      date: "October 30th, 2022",
+      location: "Brooklyn, NY",
+      venue: "Brooklyn Steel",
+      group: "Mdou Moctar"
+    },
+    {
+      upcoming: true,
+      date: "October 31st, 2022",
+      location: "Providence, RI",
+      venue: "AS220",
+      group: "Mdou Moctar"
+    },
+    {
+      upcoming: false,
+      date: "September 10th, 2022",
+      location: "London, UK",
+      venue: "Cafe Oto",
+      group: "Janka Nabay"
+    },
+  ]
+
+
+
   return (
     <div>
         <div className='subtitle'>live</div>
         <div className='spacer'></div>
         <h3>UPCOMING</h3>
-        <p>October 30th, 2022<span> : </span>Brooklyn, NY <span> @ </span> Brooklyn Steel w/ Mdou Moctar</p>
-        <p>October 30th, 2022<span> : </span>Brooklyn, NY <span> @ </span> Brooklyn Steel w/ Mdou Moctar</p>
+        {showList.map((show) => {
+          if (show.upcoming === true) {
+            return (
+            <p>{show.date} : {show.location} @ {show.venue} w/ {show.group}</p>)
+          } 
+          return null
+        })}
 
         <h3>PAST</h3>
 
-        <p>October 30th, 2022<span> : </span>Brooklyn, NY <span> @ </span> Brooklyn Steel w/ Mdou Moctar</p>
-        <p>October 30th, 2022<span> : </span>Brooklyn, NY <span> @ </span> Brooklyn Steel w/ Mdou Moctar</p>
-        <p>October 30th, 2022<span> : </span>Brooklyn, NY <span> @ </span> Brooklyn Steel w/ Mdou Moctar</p>
+         {showList.map((show) => {
+          if (show.upcoming === false) {
+            return (
+            <p>{show.date} : {show.location} @ {show.venue} w/ {show.group}</p>)
+          } 
+          return null
+        })}
     
 
         <div className='spacer'></div>
